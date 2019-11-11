c1fffa8b939e97a7daa2b6939f12a707cecceea8
Alex <alexandre.seneviratne@gmail.com>
2019-11-11 18:36:33 +0100

    getFavoriteNeighbours() and favoriteNeighbour() added

--- app/src/main/java/com/openclassrooms/entrevoisins/service/NeighbourApiService.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/service/NeighbourApiService.java
@@ -17,8 +17,22 @@ public interface NeighbourApiService {
     List<Neighbour> getNeighbours();
 
     /**
+     * Get all my favorite Neighbours
+     * @return {@link List}
+     */
+    List<Neighbour> getFavoriteNeighbours();
+
+    /**
      * Deletes a neighbour
      * @param neighbour
      */
     void deleteNeighbour(Neighbour neighbour);
+
+
+    /**
+     * SetFqvorite a neighbour
+     * @param neighbour
+     */
+    void favoriteNeighbour(Neighbour neighbour, Boolean isFavorite);
+
 }
