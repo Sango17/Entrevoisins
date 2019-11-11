2353c15328f39b1dbe26eaec8ed0c993c92bb825
Alex <alexandre.seneviratne@gmail.com>
2019-11-11 18:37:16 +0100

    Small modifs

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/adapter/ListNeighbourPagerAdapter.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/adapter/ListNeighbourPagerAdapter.java
@@ -1,11 +1,15 @@
-package com.openclassrooms.entrevoisins.ui.neighbour_list;
+package com.openclassrooms.entrevoisins.ui.neighbour_list.adapter;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> fragmentList = new ArrayList<>();
 
     public ListNeighbourPagerAdapter(FragmentManager fm) {
         super(fm);
@@ -18,7 +22,7 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
      */
     @Override
     public Fragment getItem(int position) {
-        return NeighbourFragment.newInstance();
+        return fragmentList.get(position);
     }
 
     /**
@@ -27,6 +31,10 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
      */
     @Override
     public int getCount() {
-        return 1;
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragmentList.add(fragment);
     }
 }
